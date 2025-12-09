11a44d2fe23fcc2130cb0ab272b58ef91db184f9
CesarRDF <58219887+CesarRDF@users.noreply.github.com>
2025-12-10 00:26:16 +0100

    test

--- boards/shields/nice_oled/widgets/animation.c
+++ boards/shields/nice_oled/widgets/animation.c
@@ -19,15 +19,15 @@ LV_IMG_DECLARE(crystal_09);
 LV_IMG_DECLARE(crystal_10);
 LV_IMG_DECLARE(crystal_11);
 LV_IMG_DECLARE(crystal_12);
-LV_IMG_DECLARE(crystal_13);
-LV_IMG_DECLARE(crystal_14);
-LV_IMG_DECLARE(crystal_15);
-LV_IMG_DECLARE(crystal_16);
+// LV_IMG_DECLARE(crystal_13);
+// LV_IMG_DECLARE(crystal_14);
+// LV_IMG_DECLARE(crystal_15);
+// LV_IMG_DECLARE(crystal_16);
 
 const lv_img_dsc_t *crystal_imgs[] = {
     &crystal_01, &crystal_02, &crystal_03, &crystal_04, &crystal_05, &crystal_06,
     &crystal_07, &crystal_08, &crystal_09, &crystal_10, &crystal_11, &crystal_12,
-    &crystal_13, &crystal_14, &crystal_15, &crystal_16,
+    // &crystal_13, &crystal_14, &crystal_15, &crystal_16,
 };
 
 // CONFIG_NICE_OLED_POKEMON_ANIMATION
