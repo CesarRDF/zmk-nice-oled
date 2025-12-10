b10a6a1615353b57360facf81f0b729b46231a5e
CesarRDF <58219887+CesarRDF@users.noreply.github.com>
2025-12-10 18:09:56 +0100

    implemennt kirby in kconfig

--- boards/shields/nice_oled/widgets/animation.c
+++ boards/shields/nice_oled/widgets/animation.c
@@ -144,7 +144,7 @@ void draw_animation(lv_obj_t *canvas, struct zmk_widget_screen *widget) {
     lv_obj_center(art);
 
     lv_animimg_set_src(art, (const void **)kirby_imgs, 31);
-    lv_animimg_set_duration(art, CONFIG_NICE_OLED_POKEMON_ANIMATION_MS);
+    lv_animimg_set_duration(art, CONFIG_NICE_OLED_KIRBY_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
 
